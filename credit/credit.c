cd38ddde1c129ebe99d5c2e29963b8cc11334ca1
michael-hoang <mike081522@gmail.com>
2023-01-12 21:58:54 -0800

    Fix credit. Now 100%

--- credit/credit.c
+++ credit/credit.c
@@ -11,88 +11,85 @@ int main(void)
     }
     while (cc_num < 1);
 
-    // 1. Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+    long cc_num_copy = cc_num;
 
+    // Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+    // Add the sum to the sum of the digits that weren’t multiplied by 2.
     int last_digit;
-    int i = 1; // indicator to multiply every other digit by 2 if 'i' is even
-    long remaining_cc_num = cc_num;
-    int sum_of_all_product_digits = 0;
-    int sum_of_all_non_product_digits = 0;
+    int i = 1;
+    int sum_of_all_digits = 0;
     do
     {
-        last_digit = remaining_cc_num % 10;
+        // printf("i: %i, ", i);
+        last_digit = cc_num % 10;
+        // printf("last_digit: %i\n", last_digit);
+        cc_num = (cc_num - last_digit) / 10;
         if (i % 2 == 0)
         {
             int last_digit_x2 = last_digit * 2;
-            int product_last_digit;
-            int sum_of_product_digits = 0;
-
-            do
+            // printf("last_digit_x2: %i\n", last_digit_x2);
+            last_digit = last_digit_x2;
+            if (last_digit_x2 > 9)
             {
-                product_last_digit = last_digit_x2 % 10;
-                sum_of_product_digits += product_last_digit;
-                int product_minus_lastDigit = last_digit_x2 - product_last_digit;
-                last_digit_x2 = product_minus_lastDigit / 10;
+                int primary_last_digit = last_digit_x2 % 10;
+                int secondary_last_digit = (last_digit_x2 - primary_last_digit) / 10;
+                last_digit = primary_last_digit + secondary_last_digit;
             }
-            while (product_last_digit != last_digit_x2);
-
-            sum_of_all_product_digits += sum_of_product_digits;
-        }
-        else
-        {
-            sum_of_all_non_product_digits += last_digit;
         }
 
-        long cc_minus_lastDigit = remaining_cc_num - last_digit;
-        remaining_cc_num = cc_minus_lastDigit / 10;
+        // printf("last_digit: %i\n", last_digit);
+        sum_of_all_digits += last_digit;
+        last_digit = 0;
         i++;
     }
-    while (last_digit != remaining_cc_num);
+    while (cc_num > 0);
+    // printf("%i\n", sum_of_all_digits);
 
-    // printf("%i\n", sum_of_all_product_digits);
+    // If total's last digit is 0, number is valid.
+    int is_last_digit_0 = sum_of_all_digits % 10;
 
-    // 2. Add the sum to the sum of the digits that weren’t multiplied by 2.
-
-    int sum_total = sum_of_all_product_digits + sum_of_all_non_product_digits;
-
-    // printf("%i\n", sum_total);
-
-    // 3. If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
-
-    // Get first 2 digits of credit card number.
+    // Get first two digits of cc_num
+    long cc_num_remain = cc_num_copy;
     do
     {
-        long remainder = cc_num % 10;
-        long cc_num_minus_remainder = cc_num - remainder;
-        cc_num = cc_num_minus_remainder / 10;
+        last_digit = cc_num_remain % 10;
+        cc_num_remain = (cc_num_remain - last_digit) / 10;
     }
-    while (cc_num > 100);
+    while (cc_num_remain > 100);
+
+    // printf("%ld", cc_num_remain);
+
+    // Get length of credit card
+    int cc_length = i - 1;
+
+    // printf("%i\n", cc_length);
 
     // Credit card network validation.
     string card_network;
-    if (cc_num == 34 || cc_num == 37)
+
+    if (is_last_digit_0 == 0)
     {
-        card_network = "AMEX";
-    }
-    else if (cc_num >= 40 && cc_num < 50)
-    {
-        card_network = "VISA";
-    }
-    else if (cc_num > 50 && cc_num < 56)
-    {
-        card_network = "MASTERCARD";
+        if ((cc_num_remain == 34 || cc_num_remain == 37) && (cc_length == 15))
+        {
+            card_network = "AMEX";
+        }
+        else if ((cc_num_remain >= 40 && cc_num_remain < 50) && (cc_length == 13 || cc_length == 16))
+        {
+            card_network = "VISA";
+        }
+        else if ((cc_num_remain > 50 && cc_num_remain < 56) && (cc_length == 16))
+        {
+            card_network = "MASTERCARD";
+        }
+        else
+        {
+            card_network = "INVALID";
+        }
     }
     else
     {
         card_network = "INVALID";
     }
 
-    if (sum_total % 10 == 0)
-    {
-        printf("%s\n", card_network);
-    }
-    else
-    {
-        printf("INVALID\n");
-    }
+    printf("%s\n", card_network);
 }
