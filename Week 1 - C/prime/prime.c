1cc6d42e5a87c3d50ec7d92df7f459416e46ce3d
michael-hoang <mike081522@gmail.com>
2023-01-25 00:21:13 -0800

    Complete week 1 practice problems

---
+++ Week 1 - C/prime/prime.c
@@ -0,0 +1,57 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool prime(int number);
+
+int main(void)
+{
+    int min;
+    do
+    {
+        min = get_int("Minimum: ");
+    }
+    while (min < 1);
+
+    int max;
+    do
+    {
+        max = get_int("Maximum: ");
+    }
+    while (min >= max);
+
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
+        }
+    }
+}
+
+bool prime(int number)
+{
+    // TODO
+    int i = 2;
+    do
+    {
+        if (number == 2)
+        {
+            return true;
+        }
+        else
+        {
+            int n = number % i;
+            if (n == 0)
+            {
+                return false;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+    }
+    while (i < number);
+    return true;
+}
