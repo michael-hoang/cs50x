ab4f1e0875e98a51ea4f2c7b9286c17a5a6684fd
michael-hoang <mike081522@gmail.com>
2023-01-31 01:39:43 -0800

    Complete smiley

---
+++ smiley/helpers.c
@@ -0,0 +1,17 @@
+#include "helpers.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Change all black pixels to a color of your choosing
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // RGBTRIPLE pixel = image[i][j];
+            if (image[i][j].rgbtRed == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtBlue == 0)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
+        }
+    }
+}
