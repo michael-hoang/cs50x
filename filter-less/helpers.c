74bbf723f52e356418220bf3b48e6b3c1ff51680
michael-hoang <mike081522@gmail.com>
2023-02-04 22:09:40 -0800

    Refactor blur and reflect. Thanks Jin!

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,10 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // BYTE is just an alias for uint8_t, which is an unsigned integer of size 1 byte (8 bits). Can hold values 0 - 255.
-            // BYTE avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
             int avg = (float)(image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3 + 0.5;
-
             image[i][j].rgbtBlue = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtRed = avg;
@@ -61,22 +58,15 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++) // iterate row by row
     {
-        int halfWidth = width / 2;
-        for (int j = 0, k = width - 1; j < halfWidth; j++, k--)
+        int midpoint = width / 2;
+        for (int j = 0, k = width - 1; j < midpoint; j++, k--) // iterate column by column until midpoint
         {
-            BYTE redTemp = image[i][j].rgbtRed;
-            BYTE greenTemp = image[i][j].rgbtGreen;
-            BYTE blueTemp = image[i][j].rgbtBlue;
-
-            image[i][j].rgbtRed = image[i][k].rgbtRed;
-            image[i][j].rgbtGreen = image[i][k].rgbtGreen;
-            image[i][j].rgbtBlue = image[i][k].rgbtBlue;
-
-            image[i][k].rgbtRed = redTemp;
-            image[i][k].rgbtGreen = greenTemp;
-            image[i][k].rgbtBlue = blueTemp;
+            // swap pixels at positions j and k
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][k];
+            image[i][k] = temp;
         }
     }
     return;
@@ -85,88 +75,44 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE imageTemp[height][width];
-    int avgRed, avgGreen, avgBlue;
-    for (int i = 0; i < height; i++)
+    RGBTRIPLE imageCopy[height][width];
+    for (int i = 0; i < height; i++) // iterate row by row
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++) // iterate column by column
         {
-            if (i > 0 && i < height - 1) // rows between the first and last
+            int totalRed = 0;
+            int totalGreen = 0;
+            int totalBlue = 0;
+            int totalPixels = 0;
+            for (int m = i - 1; m < i + 2; m++) // iterate row by row on a 3x3 grid
             {
-                if (j > 0 && j < width - 1) // pixels between the first and last
+                for (int n = j - 1; n < j + 2; n++) // iterate column by column on a 3x3 grid
                 {
-                    avgRed = (float)(image[i][j].rgbtRed + image[i][j + 1].rgbtRed + image[i + 1][j].rgbtRed + image[i + 1][j + 1].rgbtRed + image[i][j - 1].rgbtRed + image[i + 1][j - 1].rgbtRed + image[i - 1][j - 1].rgbtRed + image[i - 1][j].rgbtRed + image[i - 1][j + 1].rgbtRed) / 9 + 0.5;
-                    avgGreen = (float)(image[i][j].rgbtGreen + image[i][j + 1].rgbtGreen + image[i + 1][j].rgbtGreen + image[i + 1][j + 1].rgbtGreen + image[i][j - 1].rgbtGreen + image[i + 1][j - 1].rgbtGreen + image[i - 1][j - 1].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j + 1].rgbtGreen) / 9 + 0.5;
-                    avgBlue = (float)(image[i][j].rgbtBlue + image[i][j + 1].rgbtBlue + image[i + 1][j].rgbtBlue + image[i + 1][j + 1].rgbtBlue + image[i][j - 1].rgbtBlue + image[i + 1][j - 1].rgbtBlue + image[i - 1][j - 1].rgbtBlue + image[i - 1][j].rgbtBlue + image[i - 1][j + 1].rgbtBlue) / 9 + 0.5;
-                }
-                else if (j == 0) // first pixel (left edge)
-                {
-                    avgRed = (float)(image[i][j].rgbtRed + image[i][j + 1].rgbtRed + image[i + 1][j].rgbtRed + image[i + 1][j + 1].rgbtRed + image[i - 1][j].rgbtRed + image[i - 1][j + 1].rgbtRed) / 6 + 0.5;
-                    avgGreen = (float)(image[i][j].rgbtGreen + image[i][j + 1].rgbtGreen + image[i + 1][j].rgbtGreen + image[i + 1][j + 1].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j + 1].rgbtGreen) / 6 + 0.5;
-                    avgBlue = (float)(image[i][j].rgbtBlue + image[i][j + 1].rgbtBlue + image[i + 1][j].rgbtBlue + image[i + 1][j + 1].rgbtBlue + image[i - 1][j].rgbtBlue + image[i - 1][j + 1].rgbtBlue) / 6 + 0.5;
-                }
-                else // last pixel (right edge)
-                {
-                    avgRed = (float)(image[i][j].rgbtRed + image[i][j - 1].rgbtRed + image[i - 1][j].rgbtRed + image[i - 1][j - 1].rgbtRed + image[i + 1][j - 1].rgbtRed + image[i + 1][j].rgbtRed) / 6 + 0.5;
-                    avgGreen = (float)(image[i][j].rgbtGreen + image[i][j - 1].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j - 1].rgbtGreen + image[i + 1][j - 1].rgbtGreen + image[i + 1][j].rgbtGreen) / 6 + 0.5;
-                    avgBlue = (float)(image[i][j].rgbtBlue + image[i][j - 1].rgbtBlue + image[i - 1][j].rgbtBlue + image[i - 1][j - 1].rgbtBlue + image[i + 1][j - 1].rgbtBlue + image[i + 1][j].rgbtBlue) / 6 + 0.5;
-                }
-            }
-            else if (i == 0) // first row
-            {
-                if (j > 0 && j < width - 1) // pixels between first and last (top edge)
-                {
-                    avgRed = (float)(image[i][j].rgbtRed + image[i][j + 1].rgbtRed + image[i + 1][j].rgbtRed + image[i + 1][j + 1].rgbtRed + image[i][j - 1].rgbtRed + image[i + 1][j - 1].rgbtRed) / 6 + 0.5;
-                    avgGreen = (float)(image[i][j].rgbtGreen + image[i][j + 1].rgbtGreen + image[i + 1][j].rgbtGreen + image[i + 1][j + 1].rgbtGreen + image[i][j - 1].rgbtGreen + image[i + 1][j - 1].rgbtGreen) / 6 + 0.5;
-                    avgBlue = (float)(image[i][j].rgbtBlue + image[i][j + 1].rgbtBlue + image[i + 1][j].rgbtBlue + image[i + 1][j + 1].rgbtBlue + image[i][j - 1].rgbtBlue + image[i + 1][j - 1].rgbtBlue) / 6 + 0.5;
-                }
-                else if (j == 0) // first pixel (top left corner)
-                {
-                    avgRed = (float)(image[i][j].rgbtRed + image[i][j + 1].rgbtRed + image[i + 1][j].rgbtRed + image[i + 1][j + 1].rgbtRed) / 4 + 0.5;
-                    avgGreen = (float)(image[i][j].rgbtGreen + image[i][j + 1].rgbtGreen + image[i + 1][j].rgbtGreen + image[i + 1][j + 1].rgbtGreen) / 4 + 0.5;
-                    avgBlue = (float)(image[i][j].rgbtBlue + image[i][j + 1].rgbtBlue + image[i + 1][j].rgbtBlue + image[i + 1][j + 1].rgbtBlue) / 4 + 0.5;
-                }
-                else // last pixel (top right corner)
-                {
-                    avgRed = (float)(image[i][j].rgbtRed + image[i][j - 1].rgbtRed + image[i + 1][j].rgbtRed + image[i + 1][j - 1].rgbtRed) / 4 + 0.5;
-                    avgGreen = (float)(image[i][j].rgbtGreen + image[i][j - 1].rgbtGreen + image[i + 1][j].rgbtGreen + image[i + 1][j - 1].rgbtGreen) / 4 + 0.5;
-                    avgBlue = (float)(image[i][j].rgbtBlue + image[i][j - 1].rgbtBlue + image[i + 1][j].rgbtBlue + image[i + 1][j - 1].rgbtBlue) / 4 + 0.5;
-                }
-            }
-            else // last row
-            {
-                if (j > 0 && j < width - 1) // pixels between first and last (bottom edge)
-                {
-                    avgRed = (float)(image[i][j].rgbtRed + image[i][j + 1].rgbtRed + image[i - 1][j].rgbtRed + image[i - 1][j + 1].rgbtRed + image[i][j - 1].rgbtRed + image[i - 1][j - 1].rgbtRed) / 6 + 0.5;
-                    avgGreen = (float)(image[i][j].rgbtGreen + image[i][j + 1].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j + 1].rgbtGreen + image[i][j - 1].rgbtGreen + image[i - 1][j - 1].rgbtGreen) / 6 + 0.5;
-                    avgBlue = (float)(image[i][j].rgbtBlue + image[i][j + 1].rgbtBlue + image[i - 1][j].rgbtBlue + image[i - 1][j + 1].rgbtBlue + image[i][j - 1].rgbtBlue + image[i - 1][j - 1].rgbtBlue) / 6 + 0.5;
-                }
-                else if (j == 0) // first pixel (bottom left corner)
-                {
-                    avgRed = (float)(image[i][j].rgbtRed + image[i][j + 1].rgbtRed + image[i - 1][j].rgbtRed + image[i - 1][j + 1].rgbtRed) / 4 + 0.5;
-                    avgGreen = (float)(image[i][j].rgbtGreen + image[i][j + 1].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j + 1].rgbtGreen) / 4 + 0.5;
-                    avgBlue = (float)(image[i][j].rgbtBlue + image[i][j + 1].rgbtBlue + image[i - 1][j].rgbtBlue + image[i - 1][j + 1].rgbtBlue) / 4 + 0.5;
-                }
-                else // last pixel (bottom right corner)
-                {
-                    avgRed = (float)(image[i][j].rgbtRed + image[i][j - 1].rgbtRed + image[i - 1][j].rgbtRed + image[i - 1][j - 1].rgbtRed) / 4 + 0.5;
-                    avgGreen = (float)(image[i][j].rgbtGreen + image[i][j - 1].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j - 1].rgbtGreen) / 4 + 0.5;
-                    avgBlue = (float)(image[i][j].rgbtBlue + image[i][j - 1].rgbtBlue + image[i - 1][j].rgbtBlue + image[i - 1][j - 1].rgbtBlue) / 4 + 0.5;
+                    if (m >= 0 && m < height && n >= 0 && n < width) // set boundaries within image dimensions
+                    {
+                        totalRed += image[m][n].rgbtRed;
+                        totalGreen += image[m][n].rgbtGreen;
+                        totalBlue += image[m][n].rgbtBlue;
+                        totalPixels++;
+                    }
                 }
             }
 
-            imageTemp[i][j].rgbtRed = avgRed;
-            imageTemp[i][j].rgbtGreen = avgGreen;
-            imageTemp[i][j].rgbtBlue = avgBlue;
+            int averageRed = (float)totalRed / totalPixels + 0.5;
+            int averageGreen = (float)totalGreen / totalPixels + 0.5;
+            int averageBlue = (float)totalBlue / totalPixels + 0.5;
+
+            imageCopy[i][j].rgbtRed = averageRed;
+            imageCopy[i][j].rgbtGreen = averageGreen;
+            imageCopy[i][j].rgbtBlue = averageBlue;
         }
     }
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtRed = imageTemp[i][j].rgbtRed;
-            image[i][j].rgbtGreen = imageTemp[i][j].rgbtGreen;
-            image[i][j].rgbtBlue = imageTemp[i][j].rgbtBlue;
+            image[i][j] = imageCopy[i][j];
         }
     }
     return;
